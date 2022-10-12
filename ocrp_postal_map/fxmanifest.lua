4523ced6b661a7bfebb1761183e3211d82bb2578
Nick Reagan <52389274+NickReagan@users.noreply.github.com>
2022-10-12 18:32:39 -0400

    Update fxmanifest.lua
    Added new 'hideminimap' and 'showminimap' exports([suggestion](https://github.com/ocrp/postal_map/issues/6)) and changed to latest fx version.

--- ocrp_postal_map/fxmanifest.lua
+++ ocrp_postal_map/fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'gta5'
 description 'OCRP Based Postal Map for FiveM (Forked and Fixed)'
 author 'ocrp + nick_reagan'
@@ -6,3 +6,8 @@ author 'ocrp + nick_reagan'
 this_is_a_map 'yes'
 
 client_script 'client.lua'
+
+exports {
+	'hideminimap',
+  'showminimap'
+}
