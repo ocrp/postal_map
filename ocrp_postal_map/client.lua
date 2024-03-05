17597cb6a3295397c8ae2a3c0549f5f077c1e326
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2024-03-05 16:27:32 -0500

    remove citizen.

--- ocrp_postal_map/client.lua
+++ ocrp_postal_map/client.lua
@@ -1,4 +1,4 @@
-Citizen.CreateThread(function()
+CreateThread(function()
     SetMapZoomDataLevel(0, 0.96, 0.9, 0.08, 0.0, 0.0) -- Level 0
     SetMapZoomDataLevel(1, 1.6, 0.9, 0.08, 0.0, 0.0) -- Level 1
     SetMapZoomDataLevel(2, 8.6, 0.9, 0.08, 0.0, 0.0) -- Level 2
@@ -6,9 +6,9 @@ Citizen.CreateThread(function()
     SetMapZoomDataLevel(4, 22.3, 0.9, 0.08, 0.0, 0.0) -- Level 4
 end)
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-		Citizen.Wait(1)
+		Wait(1)
 		if IsPedOnFoot(GetPlayerPed(-1)) then 
 			SetRadarZoom(1100)
 		elseif IsPedInAnyVehicle(GetPlayerPed(-1), true) then
